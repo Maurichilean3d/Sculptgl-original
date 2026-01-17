7930500c1682b2e9c81611467f8f9c815958d6f4
Maurichilean3d <Maurichilean3d@users.noreply.github.com>
2026-01-17 00:48:08 -0300

    Build and deploy GitHub Pages output

---
+++ webpack.github.config.js
@@ -0,0 +1,43 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
+module.exports = function () {
+  var config = {
+    entry: './main.js',
+    output: {
+      library: 'sculptgl',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'docs'),
+      filename: 'sculptgl.js',
+      publicPath: './'
+    },
+    resolve: {
+      modules: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'lib'),
+        path.join(__dirname, 'node_modules')
+      ]
+    },
+    module: {
+      rules: [{
+        test: /\.glsl$/,
+        loader: 'raw-loader'
+      }]
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'tools/authSuccess.html', to: 'authSuccess.html' },
+          { from: 'tools/index.github.html', to: 'index.html' },
+          { from: 'app/css', to: 'css' },
+          { from: 'app/resources', to: 'resources' },
+          { from: 'app/worker', to: 'worker' }
+        ],
+      })
+    ]
+  };
+
+  config.mode = 'production';
+
+  return config;
+};
